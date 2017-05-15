f6b529f7446ef7c82f4f8613a179e87bbcfdfe98
住田淳 <sj0815.freedom@docomo.ne.jp>
2017-05-15 16:49:30 +0900

    バリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+validates :mail_address, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+validates :name, presence: true
+validates :password, presence: true, length: { minimum: 8, message: "8文字以上で入力してください" }, format: { with: /\A[a-z0-9]+\z/i, message: "英数字で入力してください" }
 end
