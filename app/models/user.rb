4297fc3962fcccdcf64f796defdb38b0c0965aff
住田淳 <sj0815.freedom@docomo.ne.jp>
2017-05-29 18:00:32 +0900

    deviseの導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable,
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 validates :mail_address, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
 validates :name, presence: true
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :log_users
+  root 'pages#index'
+
+  get 'pages/show'
+
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
