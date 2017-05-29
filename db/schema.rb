1e15b6b809e8fe8c48b5de2fbced3d6c7a0c1200
住田淳 <sj0815.freedom@docomo.ne.jp>
2017-05-29 13:25:58 +0900

    sqlite3からmysql2に変更

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 20170515054510) do
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "mail_address"
     t.string   "name"
     t.string   "password"
