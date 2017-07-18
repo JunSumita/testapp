20bc875242aa0635b01d95c51cec88a563986dac
住田淳 <sj0815.freedom@docomo.ne.jp>
2017-07-18 17:30:37 +0900

    category_treeの実装

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,9 +11,9 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery.turbolinks
 //= require jquery_ujs
+//= require jquery.turbolinks
 //= require twitter/bootstrap
 //= require select2
-//= require turbolinks
+//= require jstree.min
 //= require_tree .
--- app/models/category.rb
+++ app/models/category.rb
@@ -3,4 +3,26 @@ class Category < ApplicationRecord
 
     has_many :user_category_relationships, dependent: :destroy, foreign_key: "category_id"
     has_many :users , through: :user_category_relationships, inverse_of: :category
+
+    # ルートノードに移動させた場合は、parent_id は "#"になるので、
+  # ルートに移動させる move_to_root メソッドを呼ぶ
+  # 親ノードがある場合は、parent_id を 更新する
+  def parent_id=(parent_id)
+    if parent_id == '#'
+      move_to_root
+    else
+      super(parent_id)
+    end
+  end
+
+  # ルートノードに移動させた場合は、親ノード(parent)がnilのため、ルートの兄弟配列から移動先を特定する
+  # 親ノードがある場合は、move_to_child_with_indexメソッドで移動する
+  def new_position=(new_position)
+    if parent.blank?
+      prev_node = root.siblings[new_position.to_i - 1]
+      move_to_right_of prev_node
+    else
+      move_to_child_with_index(parent, new_position.to_i)
+    end
+  end
 end
